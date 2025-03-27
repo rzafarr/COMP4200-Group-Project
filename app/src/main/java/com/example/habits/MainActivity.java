3c15aa38f21db345922c364bb22f12d218fd0324
Josiah Henson <pbinspanish@outlook.com>
2025-03-27 11:27:44 -0400

    remove unused imports and fix some warnings

--- app/src/main/java/com/example/habits/MainActivity.java
+++ app/src/main/java/com/example/habits/MainActivity.java
@@ -1,43 +1,15 @@
 package com.example.habits;
 
-import static android.text.format.DateUtils.isToday;
-
-import android.annotation.SuppressLint;
-import android.app.AlarmManager;
-import android.app.AlertDialog;
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.os.Build;
 import android.os.Bundle;
-import android.text.InputType;
-import android.util.Log;
-import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.chip.Chip;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -50,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
         // set up navigation
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
-        NavController navController = navHostFragment.getNavController();
+        NavController navController = Objects.requireNonNull(navHostFragment).getNavController();
 
         // set up the navigation view
         BottomNavigationView navigationView = findViewById(R.id.navigation);
@@ -58,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
             navigationView.getMenu().findItem(R.id.item_1).setIcon(R.drawable.home_outline);
             navigationView.getMenu().findItem(R.id.item_2).setIcon(R.drawable.settings_outline);
 
-            switch (item.getTitle().toString()) {
+            switch (Objects.requireNonNull(item.getTitle()).toString()) {
                 case "Home":
                     item.setIcon(R.drawable.home_filled);
                     navController.navigate(R.id.habitListFragment);
